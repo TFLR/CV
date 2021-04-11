cf75112e2b5f80624c65f562dbe05f9133f4a94f
TFLR <xrxstyyy@outlook.fr>
2021-04-11 20:15:06 +0200

    final

--- js/script.js
+++ js/script.js
@@ -1,51 +1,60 @@
-$(window).scroll(function () {
-
+$(window)
+  .scroll(function () {
     var $window = $(window),
-        $body = $('body'),
-        $panel = $('.panel');
-    $panel1 = $('.panel1');
+      $body = $("body"),
+      $panel = $(".panel");
+    $panel1 = $(".panel1");
 
-    var scroll = $window.scrollTop() + ($window.height() / 3);
+    var scroll = $window.scrollTop() + $window.height() / 3;
 
     $panel.each(function () {
-        var $this = $(this);
-        if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-            $body.removeClass(function (index, css) {
-                return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
-            });
-            $body.addClass('color-' + $(this).data('color'));
-        }
+      var $this = $(this);
+      if (
+        $this.position().top <= scroll &&
+        $this.position().top + $this.height() > scroll
+      ) {
+        $body.removeClass(function (index, css) {
+          return (css.match(/(^|\s)color-\S+/g) || []).join(" ");
+        });
+        $body.addClass("color-" + $(this).data("color"));
+      }
     });
+  })
+  .scroll();
 
-}).scroll();
-
-var typed = new Typed('.typed', {
-    strings: [
-        "Developper.",
-        "Designer."
-    ],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true
+var typed = new Typed(".typed", {
+  strings: ["Developer.", "Designer."],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
 });
 
 new kursor({
-    type: 1,
-    removeDefaultCursor: true
+  type: 1,
+  removeDefaultCursor: true,
 });
 
 $(".contact").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#contact").offset().top
-    }, 1500);
+  $("html, body").animate(
+    {
+      scrollTop: $("#contact").offset().top,
+    },
+    1500
+  );
 });
 $(".about").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#about").offset().top
-    }, 1500);
+  $("html, body").animate(
+    {
+      scrollTop: $("#about").offset().top,
+    },
+    1500
+  );
 });
 $(".portfolio").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#portfolio").offset().top
-    }, 1500);
+  $("html, body").animate(
+    {
+      scrollTop: $("#portfolio").offset().top,
+    },
+    1500
+  );
 });
